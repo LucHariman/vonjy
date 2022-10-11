1da77c5f7260c1af19a201c1a6bdd5dd26832149
Luc Hariman Randrianomenjanahary <luchariman@gmail.com>
2022-10-11 20:56:09 +0200

    feat: store and retrieve client details on Firestore

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,10 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<any> {
+    // TODO: Remove this sample code
+    const clientId = '9f71a9da-4991-11ed-b878-0242ac120002';
+    const details = await this.appService.queryClientDetails(clientId);
+    return JSON.stringify(details);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
+import { firestore } from 'firebase-admin';
+
+export interface ClientDetails {
+  clientSecret: string;
+  serverUrl: string;
+}
+
+const SPACE_APP_INSTANCES_COLLECTION = 'spaceAppInstances';
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  async storeClientDetails(clientId: string, details: ClientDetails): Promise<void> {
+    const collection = firestore().collection(SPACE_APP_INSTANCES_COLLECTION);
+    await collection.doc(clientId).set(details);
+  }
+
+  async queryClientDetails(clientId: string): Promise<ClientDetails | undefined> {
+    const collection = firestore().collection(SPACE_APP_INSTANCES_COLLECTION);
+    const snapshot = await collection.doc(clientId).get();
+    return snapshot.exists ? snapshot.data() as ClientDetails : undefined;
   }
 }
